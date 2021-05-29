ded079e50f7a88d2df7f084ecdcfe36de3abe735
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2021-05-29 08:02:04 +0200

    Add files via upload

---
+++ Tpl/Page.tpl.php
@@ -0,0 +1,155 @@
+								<!-- free iperbooking form -->
+								<!-- free iperbooking form -->
+								<form action="https://www.iperbooking.com/free/iperbooking-form.cfm" method="post">
+								<!-- 																 -->
+								<!-- INSERIRE AL POSTO DEI CARATTERI ::EEEEEEEE:: L'EMAIL DELL'HOTEL -->
+								<!-- 																 -->
+								<input type="hidden" name="EMAILHOTEL" value="::EEEEEEEE::">
+								<!-- 																								 -->
+								<!-- INSERIRE AL POSTO DEI CARATTERI ::WWWWWWW:: L'INDIRIZZO DEL SITO DELL'HOTEL COMPLETO DI HTTP:// -->
+								<!-- 																								 -->
+								<input type="hidden" name="Risposta" value="::WWWWWWW::">
+								
+								<table width="500" border="0">
+									<tr>
+										<td> </td>
+										<td colspan="2"><b>modulo richiesta di prenotazione Hotel</b><br></td>
+									</tr>
+									<tr>
+										<td> </td>
+										<td>  * i campi contrassegnati dall'asterisco sono obbligatori<br></td>
+									</tr>
+									<tr> 
+										<td align="right"> * nome: </td>
+										<td><input type="text" name="nome" size="45" value=""></td>
+										<td> </td>
+									</tr>
+									<tr> 
+										<td align="right"> * cognome: </td>
+										<td><input type="text" name="cognome" size="45" value=""></td>
+										<td> </td>
+									</tr>
+									<tr> 
+										<td align="right"> * indirizzo: </td>
+										<td>
+											<input type="text" name="indirizzo" size="45" value="">
+										</td>
+										<td> </td>
+									</tr>
+									<tr> 
+										<td align="right"> * città: </td>
+										<td><input type="text" name="citta" size="45" value=""></td>
+										<td> </td>
+									</tr>
+									<tr>
+										<td align="right"> * paese: </td>
+										<td>
+											<select name="nazione" size="1">
+												<option value="albania">albania</option><option value="algeria">algeria</option><option value="american samoa">american samoa</option><option value="andorra">andorra</option><option value="angola">angola</option><option value="anguilla">anguilla</option><option value="antigua">antigua</option><option value="argentina">argentina</option><option value="armenia">armenia</option><option value="aruba">aruba</option><option value="australia">australia</option><option value="austria">austria</option><option value="azerbaijan">azerbaijan</option><option value="bahamas">bahamas</option><option value="bahrain">bahrain</option><option value="bangladesh">bangladesh</option><option value="barbados">barbados</option><option value="barbuda">barbuda</option><option value="belarus">belarus</option><option value="belgium">belgium</option><option value="belize">belize</option><option value="benin">benin</option><option value="bermuda">bermuda</option><option value="bhutan">bhutan</option><option value="bolivia">bolivia</option><option value="bonaire">bonaire</option><option value="botswana">botswana</option><option value="brazil">brazil</option><option value="british virgin islands">british virgin islands</option><option value="brunei">brunei</option><option value="bulgaria">bulgaria</option><option value="burkina faso">burkina faso</option><option value="burundi">burundi</option><option value="cambodia">cambodia</option><option value="cameroon">cameroon</option><option value="canada">canada</option><option value="cape verde">cape verde</option><option value="cayman islands">cayman islands</option><option value="central african republic">central african republic</option><option value="chad">chad</option><option value="channel islands">channel islands</option><option value="chile">chile</option><option value="china">china</option><option value="colombia">colombia</option><option value="congo-brazzaville">congo-brazzaville</option><option value="congo-kinshasa">congo-kinshasa</option><option value="cook islands">cook islands</option><option value="costa rica">costa rica</option><option value="croatia">croatia</option><option value="curacao">curacao</option><option value="cyprus">cyprus</option><option value="czech republic">czech republic</option><option value="denmark">denmark</option><option value="djibouti">djibouti</option><option value="dominica">dominica</option><option value="dominican republic">dominican republic</option><option value="ecuador">ecuador</option><option value="egypt">egypt</option><option value="el salvador">el salvador</option><option value="equatorial guinea">equatorial guinea</option><option value="eritrea">eritrea</option><option value="estonia">estonia</option><option value="ethiopia">ethiopia</option><option value="faroe islands">faroe islands</option><option value="fiji">fiji</option><option value="finland">finland</option><option value="france">france</option><option value="french guiana">french guiana</option><option value="french polynesia">french polynesia</option><option value="gabon">gabon</option><option value="gambia">gambia</option><option value="georgia">georgia</option><option value="germany">germany</option><option value="ghana">ghana</option><option value="gibraltar">gibraltar</option><option value="greece">greece</option><option value="greenland">greenland</option><option value="grenada">grenada</option><option value="guadeloupe">guadeloupe</option><option value="guam">guam</option><option value="guatemala">guatemala</option><option value="guinea">guinea</option><option value="guinea bissau">guinea bissau</option><option value="guyana">guyana</option><option value="haiti">haiti</option><option value="honduras">honduras</option><option value="hong kong">hong kong</option><option value="hungary">hungary</option><option value="iceland">iceland</option><option value="india">india</option><option value="indonesia">indonesia</option><option value="iran">iran</option><option value="iraq">iraq</option><option value="ireland">ireland</option><option value="israel">israel</option><option value="italia"selected>italia</option><option value="ivory coast">ivory coast</option><option value="jamaica">jamaica</option><option value="japan">japan</option><option value="jordan">jordan</option><option value="kazakhstan">kazakhstan</option><option value="kenya">kenya</option><option value="kuwait">kuwait</option><option value="kyrgyzstan">kyrgyzstan</option><option value="laos">laos</option><option value="latvia">latvia</option><option value="lebanon">lebanon</option><option value="lesotho">lesotho</option><option value="liberia">liberia</option><option value="libya">libya</option><option value="liechtenstein">liechtenstein</option><option value="lithuania">lithuania</option><option value="luxembourg">luxembourg</option><option value="macau">macau</option><option value="macedonia">macedonia</option><option value="madagascar">madagascar</option><option value="malawi">malawi</option><option value="malaysia">malaysia</option><option value="mali">mali</option><option value="malta">malta</option><option value="marshall islands">marshall islands</option><option value="martinique">martinique</option><option value="mauritania">mauritania</option><option value="mauritius">mauritius</option><option value="mexico">mexico</option><option value="micronesia">micronesia</option><option value="moldova">moldova</option><option value="monaco">monaco</option><option value="mongolia">mongolia</option><option value="montserrat">montserrat</option><option value="morocco">morocco</option><option value="mozambique">mozambique</option><option value="myanmar/burma">myanmar/burma</option><option value="namibia">namibia</option><option value="nepal">nepal</option><option value="netherlands">netherlands</option><option value="netherlands antilles">netherlands antilles</option><option value="new caledonia">new caledonia</option><option value="new zealand">new zealand</option><option value="nicaragua">nicaragua</option><option value="niger">niger</option><option value="nigeria">nigeria</option><option value="norway">norway</option><option value="oman">oman</option><option value="pakistan">pakistan</option><option value="palau">palau</option><option value="panama">panama</option><option value="papua new guinea">papua new guinea</option><option value="paraguay">paraguay</option><option value="peru">peru</option><option value="philippines">philippines</option><option value="poland">poland</option><option value="portugal">portugal</option><option value="puerto rico">puerto rico</option><option value="qatar">qatar</option><option value="reunion">reunion</option><option value="romania">romania</option><option value="russia">russia</option><option value="rwanda">rwanda</option><option value="saba">saba</option><option value="saipan">saipan</option><option value="san marino">san marino</option><option value="saudi arabia">saudi arabia</option><option value="senegal">senegal</option><option value="seychelles">seychelles</option><option value="sierra leone">sierra leone</option><option value="singapore">singapore</option><option value="slovak republic">slovak republic</option><option value="slovenia">slovenia</option><option value="somalia">somalia</option><option value="south africa">south africa</option><option value="south korea">south korea</option><option value="spain">spain</option><option value="sri lanka">sri lanka</option><option value="st. barthelemy">st. barthelemy</option><option value="st. croix">st. croix</option><option value="st. eustatius">st. eustatius</option><option value="st. kitts and nevis">st. kitts and nevis</option><option value="st. lucia">st. lucia</option><option value="st. maarten/st. martin">st. maarten/st. martin</option><option value="st. thomas">st. thomas</option><option value="st. vincent">st. vincent</option><option value="sudan">sudan</option><option value="suriname">suriname</option><option value="swaziland">swaziland</option><option value="sweden">sweden</option><option value="switzerland">switzerland</option><option value="syria">syria</option><option value="taiwan">taiwan</option><option value="tanzania">tanzania</option><option value="thailand">thailand</option><option value="togo">togo</option><option value="tortola">tortola</option><option value="trinidad and tobago">trinidad and tobago</option><option value="tunisia">tunisia</option><option value="turkey">turkey</option><option value="turkmenistan">turkmenistan</option><option value="turks and caicos islands">turks and caicos islands</option><option value="u.s. virgin islands">u.s. virgin islands</option><option value="u.s.a.">u.s.a.</option><option value="uganda">uganda</option><option value="ukraine">ukraine</option><option value="united arab emirates">united arab emirates</option><option value="united kingdom">united kingdom</option><option value="uruguay">uruguay</option><option value="uzbekistan">uzbekistan</option><option value="vanuatu">vanuatu</option><option value="vatican city">vatican city</option><option value="venezuela">venezuela</option><option value="vietnam">vietnam</option><option value="yemen">yemen</option><option value="zambia">zambia</option><option value="zimbabwe">zimbabwe</option>
+											</select>
+										</td>
+										<td> </td>
+									</tr>
+									<tr>
+										<td align="right"> cap: </td>
+										<td><input type="text" name="cap" size="15" value=""></td>
+										<td> </td>
+									</tr>
+									<tr> 
+										<td align="right"> * telefono: </td>
+										<td><input type="text" name="telefono" size="15" value=""></td>
+										<td> </td>
+									</tr>
+									<tr>
+										<td align="right">skype:</td>
+										<td><input type="text" name="skype" size="15" value=""></td>
+										<td> </td>
+									</tr>
+									<tr> 
+										<td align="right"> * e-mail: </td>
+										<td><input type="text" name="email" size="45" value=""></td>
+										<td> </td>
+									</tr>
+									<tr> 
+										<td align="right"> * arrivo: </td>
+										<td>
+											<select name="giornoarrivo"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option></select>
+											
+											<select name="mesearrivo"><option value="1">gennaio</option><option value="2">febbraio</option><option value="3">marzo</option><option value="4">aprile</option><option value="5">maggio</option><option value="6">giugno</option><option value="7">luglio</option><option value="8">agosto</option><option value="9">settembre</option><option value="10">ottobre</option><option value="11">novembre</option><option value="12">dicembre</option></select>
+											<select name="annoarrivo"><option value="2020" selected>2020</option><option value="2021" >2021</option></select>
+										</td>
+										<td> </td>
+									</tr>
+									<tr> 
+										<td align="right"> * partenza:</td>
+										<td>
+											<select name="giornopartenza"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option></select>
+											<select name="mesepartenza"><option value="1">gennaio</option><option value="2">febbraio</option><option value="3">marzo</option><option value="4">aprile</option><option value="5">maggio</option><option value="6">giugno</option><option value="7">luglio</option><option value="8">agosto</option><option value="9">settembre</option><option value="10">ottobre</option><option value="11">novembre</option><option value="12">dicembre</option></select>
+											<select name="annopartenza"><option value="2020" selected>2020</option><option value="2021" >2021</option></select>
+										</td>
+										<td> </td>
+									</tr>
+									<tr>
+										<td align="right"> * n. adulti: </td>
+										<td>
+											<select name="numeroadulti"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option></select>
+											    * n. bambini: 
+											<select name="numerobambini"><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option></select>
+										</td>
+										<td> </td>
+									</tr>
+									<tr>
+										<td align="right"> numero camere </td>
+										<td><select name="numerocamere"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option></select></td>
+										<td> </td>
+									</tr>
+									<tr>
+										<td align="right">camere </td>
+										<td>
+											<select name="camera_1">
+											
+												<option value="singola">singola</option>
+												<option value="doppiaUsoSingola">doppia uso sing.</option>
+												<option value="doppia">doppia</option>
+												<option value="tripla">tripla</option>
+												<option value="quadrupla">quadrupla</option>
+											</select>
+										</td>
+									</tr>
+								
+									<tr>
+										<td align="right">trattamento </td>
+										<td>
+											<select name="trattamento_1">
+												<option value="pernottamento e colazione">pernottamento e colazione</option>
+												<option value="pernottamento">pernottamento</option>
+												<option value="mezza pensione">mezza pensione</option>
+												<option value="pensione completa">pensione completa</option>
+												<option value="all inclusive">all inclusive</option>
+											</select>
+										</td>
+									</tr>
+									<tr>
+										<td align="right">prenotazione urgente:</td>
+										<td><input type="checkbox" name="prenotazioneurgente"></td>
+										<td> </td>
+									</tr>
+									<tr>
+										<td align="right"> note: </td>
+										<td><input type="text" name="note" size="45" value=""></td>
+										<td> </td>
+									</tr>
+									<tr>
+										<td> </td>
+										<td><br><input type="submit" value="clicca qui per richiedere la disponibilità"><br><br></td>
+										<td> </td>
+									</tr>
+									 <tr>
+										<td> </td>
+										<td>
+											ogni richiesta è subordinata alla conferma dell'albergo<br><br>
+										</td>
+									</tr>
+								</table>
+								</form>
+								
